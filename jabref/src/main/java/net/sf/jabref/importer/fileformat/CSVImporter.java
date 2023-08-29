0476a9148a45862044374133aab92129687b27c7
Pedro <88565386+void-Pedro@users.noreply.github.com>
2023-08-29 20:03:28 -0300

    Create CSVImporter.java
    Criando classe para importação de CSV

---
+++ jabref/src/main/java/net/sf/jabref/importer/fileformat/CSVImporter.java
@@ -0,0 +1,62 @@
+package net.sf.jabref.importer.fileformat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arraylist;
+import java.util.List;
+import net.sf.jabref.importer.ImportFormatReader;
+import net.sf.jabref.importer.OutputPrinter;
+import net.sf.jabref.model.entry.BibEntry;
+import net.sf.jabref.model.entry.BibtexEntryTypes;
+
+public class CSVImporter extends ImportFormat {
+  @Override
+  public String getFormatName () {
+    return "CSV";
+  }
+  
+  @Override
+  public boolean isRecognizedFormat (InputStream stream) throws IOException {
+    return true;
+  }
+    
+  @override
+  public String getExtensions () {
+    return "csv";
+  }
+
+public List‹BibEntry> importEntries(InputStream stream, OutputPrinter
+printer) throws IOException {
+  List‹BibEntry> bibEntryItems = new ArrayList<>();
+  BufferedReader entryFile = new BufferedReader(ImportFormatReader.getReaderDefaultEncoding(stream));
+  String line = entryFile.readLine();
+  String[] fields;
+  
+  while (line != null) {
+    if (!line.trim().isEmpty()) {
+      fields = line.split(";");
+      BibEntry entry = new BibEntry();
+      if (fields[0].equals("book")) {
+        entry.setType (BibtexEntryTypes.BOOK);
+        entry.setField("title", fields[1]);
+        entry.setField("publisher", fields[2]);
+        entry.setField("year", fields[3]);
+        entry.setField("author", fields[4]);
+        entry.setField("editor", fields[5]);
+        entry.setField("bibtexkey", fields[6]);
+      } else if (fields [0].equals("article")) {
+        entry.setType (BibtexEntryTypes.ARTICLE);
+        entry.setField("author", fields[1]);
+        entry.setField("title", fields [2]);
+        entry.setField("journal", fields[3]);
+        entry.setField("year", fields [4]);
+        entry.setField("bibtexkey", fields[5]);
+    }
+    bibEntryItems.add(entry);
+    line = entryFile.readLine();
+    }
+  }
+  return bibEntryItems;
+}
+}
